e8a228215e6ec6d18dbbe81ac841842735e36810
Chris Mason <chrismason0043@gmail.com>
2021-11-16 21:56:18 -0500

    init commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,48 @@
+﻿//namespace HelloWorld
+//{
+//  public class Program
+//{
+//  static void Main(string[] args)
+//{
+//Console.WriteLine("Hello, World!");
+//}
+//}
+//}
+
+/*namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Chris";
+            var isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}*/
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            Pi = 1;
+            
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MaxValue, float.MaxValue);
+
+        }
+    }
+}
